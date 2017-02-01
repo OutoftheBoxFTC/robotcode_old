94926b06271b0ebfc6ea6ca62e10109911c2438e
FTC 7244 <contact@brandonbarker.me>
2017-02-01 17:27:39 -0500

    Small Refactoring

--- TeamCode/src/main/java/org/ftc7244/robotcontroller/CornerBlue.java
+++ TeamCode/src/main/java/org/ftc7244/robotcontroller/CornerBlue.java
@@ -7,21 +7,11 @@ import org.ftc7244.robotcontroller.autonomous.PIDAutonomous;
 /**
  * Created by OOTB on 11/11/2016.
  */
-
-@Autonomous(name = "Corner Blue")
+@Autonomous(name = "Corner Blue", group = "Blue")
 public class CornerBlue extends PIDAutonomous {
 
     @Override
     public void run() throws InterruptedException {
-        //ENCODER BASE
-        /*gyroscope.drive(.8, 12, 0);
-        gyroscope.drive(1, 22, 22);
-        gyroscope.rotate(.8, 55, Direction.LEFT);
-        robot.shootLoop(3, 1000);
-        gyroscope.rotate(.8, 60, Direction.RIGHT);
-        robot.getIntake().setPower(-1);
-        gyroscope.drive(1, 20, 20);*/
-
         gyroscope.drive(.5, 5);
         gyroscope.rotate(-45);
         gyroscope.drive(.5, 10);
--- TeamCode/src/main/java/org/ftc7244/robotcontroller/CornerRed.java
+++ TeamCode/src/main/java/org/ftc7244/robotcontroller/CornerRed.java
@@ -7,22 +7,11 @@ import org.ftc7244.robotcontroller.autonomous.PIDAutonomous;
 /**
  * Created by OOTB on 11/11/2016.
  */
-
-@Autonomous(name = "Corner Red")
+@Autonomous(name = "Corner Red", group = "Red")
 public class CornerRed extends PIDAutonomous {
 
     @Override
     public void run() throws InterruptedException {
-        /*sleep(5000);
-        gyroscope.drive(.8, -10, 0);
-        gyroscope.drive(1, -24, -24);
-        gyroscope.rotate(1, 35, Direction.RIGHT);
-        robot.shootLoop(3, 1000);
-        gyroscope.rotate(.8, 35, Direction.LEFT);
-        gyroscope.drive(1, -15, -15);
-        gyroscope.rotate(.8, 40, Direction.LEFT);
-        gyroscope.drive(1, -10, -10);*/
-
         gyroscope.drive(-0.5, 5);
         gyroscope.rotate(45);
         gyroscope.drive(-0.5, 5);
