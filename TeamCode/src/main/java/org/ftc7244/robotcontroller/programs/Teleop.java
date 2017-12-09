711d251c9055992fcbec3cd0309111f2af17e48d
BeaverDuck <beaverduck99@gmail.com>
2017-12-08 20:13:25 -0500

    Removed Redundant Code

--- TeamCode/src/main/java/org/ftc7244/robotcontroller/programs/Teleop.java
+++ TeamCode/src/main/java/org/ftc7244/robotcontroller/programs/Teleop.java
@@ -2,50 +2,43 @@ package org.ftc7244.robotcontroller.programs;
 
 import android.media.MediaPlayer;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.R;
 import org.ftc7244.robotcontroller.hardware.Westcoast;
 import org.ftc7244.robotcontroller.input.Button;
 import org.ftc7244.robotcontroller.input.ButtonType;
+import org.ftc7244.robotcontroller.input.PressButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Eeshwar Laptop on 10/16/2017.
  */
 
 @TeleOp(name = "Relic Recovery Westcoast: Jiggle")
-public class Teleop extends OpMode {
-    Westcoast robot;
-    private Button leftTrigger1, dPadUp, dPadDown, rightTrigger, leftTrigger, rightBumper, leftBumper, aButton, bButton, panicButton;
+public class Teleop extends LinearOpMode {
+    private Westcoast robot;
+    private Button leftTrigger1, dPadUp, dPadDown, rightTrigger, leftTrigger, rightBumper, leftBumper, aButton, bButton, panicButton, driverAButton, driverBButton, driverXButton;
     private static final double SLOW_DRIVE_COEFFICIENT = 0.5, LIFT_VERTICAL_REST = 0.1, LIFT_RAISE = .8;
     private double VERTICLE_INTAKE_MULTIPLIER = 1, VERTICLE_INTAKE_STOP = 1;
     private static final long JIGGLE_INTERVAL_MS = 250, JIGGLE_INTERVAL_REST = 500;
-    private boolean didInit = false;
     ElapsedTime elapsedTime = new ElapsedTime();
-    private MediaPlayer sound;
+    private int currentSound;
+    private ArrayList<MediaPlayer> sounds;
 
-
-    public void init(){
-        //sound = MediaPlayer.create(hardwareMap.appContext)
-        robot = new Westcoast(this);
-        panicButton = new Button(gamepad1, ButtonType.LEFT_BUMPER);
-        leftTrigger1 = new Button(gamepad1, ButtonType.LEFT_TRIGGER);
-        dPadUp = new Button(gamepad2, ButtonType.D_PAD_UP);
-        dPadDown = new Button(gamepad2, ButtonType.D_PAD_DOWN);
-        rightTrigger = new Button(gamepad2, ButtonType.RIGHT_TRIGGER);
-        leftTrigger = new Button(gamepad2, ButtonType.LEFT_TRIGGER);
-        rightBumper = new Button(gamepad2, ButtonType.RIGHT_BUMPER);
-        leftBumper = new Button(gamepad2, ButtonType.LEFT_BUMPER);
-        aButton = new Button(gamepad2, ButtonType.A);
-        bButton = new Button(gamepad2, ButtonType.B);
-        robot.init();
-    }
     /*
     Driver:
     Left Joystick: Drive Left
     Right Joystick: Drive Right
     Right Trigger: Slow Mode
+    A: NFL song
+    B: Sad song
+    X: Clear song;
 
     Operator:
     Dpad Up: Raise Intake
@@ -58,89 +51,115 @@ public class Teleop extends OpMode {
     A: Intake Up
     B: Intake Down
      */
+
     @Override
-    public void loop(){
-        if(!didInit){
-            robot.initServos();
-            didInit = true;
-        }
-        double coefficient = leftTrigger1.isPressed()?SLOW_DRIVE_COEFFICIENT:1;
-        robot.drive(-gamepad1.left_stick_y*coefficient, -gamepad1.right_stick_y*coefficient);
-        if(panicButton.isPressed()){
-            robot.getSpring().setPosition(0.6);
-        }
-        //Drive Code
-        boolean horizontalRunning = false;
-        if(rightTrigger.isPressed()){
-            horizontalRunning = true;
-            robot.getIntakeTopLeft().setPower(0.5 * VERTICLE_INTAKE_MULTIPLIER * VERTICLE_INTAKE_STOP);
-            robot.getIntakeTopRight().setPower(0.5 * VERTICLE_INTAKE_MULTIPLIER * VERTICLE_INTAKE_STOP);
-            if(elapsedTime.milliseconds() >= JIGGLE_INTERVAL_MS){
-                VERTICLE_INTAKE_STOP = 0;
+    public void runOpMode() throws InterruptedException {
+        sounds = new ArrayList<>(Arrays.asList(MediaPlayer.create(hardwareMap.appContext, R.raw.nfltheme), MediaPlayer.create(hardwareMap.appContext, R.raw.sadviolin)));
+        currentSound = 0;
+        robot = new Westcoast(this);
+        driverAButton = new Button(gamepad1, ButtonType.A);
+        driverBButton = new Button(gamepad1, ButtonType.B);
+        driverXButton = new Button(gamepad1, ButtonType.X);
+        panicButton = new Button(gamepad1, ButtonType.LEFT_BUMPER);
+        leftTrigger1 = new Button(gamepad1, ButtonType.LEFT_TRIGGER);
+        dPadUp = new Button(gamepad2, ButtonType.D_PAD_UP);
+        dPadDown = new Button(gamepad2, ButtonType.D_PAD_DOWN);
+        rightTrigger = new Button(gamepad2, ButtonType.RIGHT_TRIGGER);
+        leftTrigger = new Button(gamepad2, ButtonType.LEFT_TRIGGER);
+        rightBumper = new Button(gamepad2, ButtonType.RIGHT_BUMPER);
+        leftBumper = new Button(gamepad2, ButtonType.LEFT_BUMPER);
+        aButton = new Button(gamepad2, ButtonType.A);
+        bButton = new Button(gamepad2, ButtonType.B);
+        robot.init();
+        waitForStart();
+        robot.initServos();
+        sounds.get(currentSound).start();
+        while (opModeIsActive()){
+            double coefficient = leftTrigger1.isPressed()?SLOW_DRIVE_COEFFICIENT:1;
+            robot.drive(-gamepad1.left_stick_y*coefficient, -gamepad1.right_stick_y*coefficient);
+            if(panicButton.isPressed()){
+                robot.getSpring().setPosition(0.6);
             }
-            if(elapsedTime.milliseconds() >= JIGGLE_INTERVAL_REST){
-                VERTICLE_INTAKE_MULTIPLIER = -VERTICLE_INTAKE_MULTIPLIER;
-                VERTICLE_INTAKE_STOP = 1;
-                elapsedTime.reset();
+            //Drive Code
+            boolean horizontalRunning = false;
+            if(rightTrigger.isPressed()){
+                horizontalRunning = true;
+                robot.getIntakeTopLeft().setPower(0.5 * VERTICLE_INTAKE_MULTIPLIER * VERTICLE_INTAKE_STOP);
+                robot.getIntakeTopRight().setPower(0.5 * VERTICLE_INTAKE_MULTIPLIER * VERTICLE_INTAKE_STOP);
+                if(elapsedTime.milliseconds() >= JIGGLE_INTERVAL_MS){
+                    VERTICLE_INTAKE_STOP = 0;
+                }
+                if(elapsedTime.milliseconds() >= JIGGLE_INTERVAL_REST){
+                    VERTICLE_INTAKE_MULTIPLIER = -VERTICLE_INTAKE_MULTIPLIER;
+                    VERTICLE_INTAKE_STOP = 1;
+                    elapsedTime.reset();
+                }
+                robot.getIntakeTopLeft().setPower(0);
+                robot.getIntakeTopRight().setPower(0);
+                robot.getIntakeBottom().setPower(-1);
             }
-            robot.getIntakeTopLeft().setPower(0);
-            robot.getIntakeTopRight().setPower(0);
-            robot.getIntakeBottom().setPower(-1);
-        }
-        else if(leftTrigger.isPressed()){
-            horizontalRunning = true;
-            robot.getIntakeBottom().setPower(1);
-        }
-        else {
-            robot.getIntakeBottom().setPower(0);
-        }
-        if(leftBumper.isPressed()){
-            horizontalRunning = true;
-            robot.getIntakeBottomLeft().setPower(0);
-            robot.getIntakeBottomRight().setPower(0);
-            robot.getIntakeTop().setPower(1);
-        }
-        else if(rightBumper.isPressed()){
-            horizontalRunning = true;
-            robot.getIntakeTop().setPower(-1);
-        }
-        else {
-            robot.getIntakeTop().setPower(0);
-        }
-        if(!horizontalRunning){
-            if(aButton.isPressed()){
-                robot.getIntakeBottomRight().setPower(.5);
-                robot.getIntakeTopRight().setPower(.5);
-                robot.getIntakeBottomLeft().setPower(.5);
-                robot.getIntakeTopLeft().setPower(.5);
-            }
-            else if(bButton.isPressed()){
-                robot.getIntakeBottomRight().setPower(-.5);
-                robot.getIntakeTopRight().setPower(-.5);
-                robot.getIntakeBottomLeft().setPower(-.5);
-                robot.getIntakeTopLeft().setPower(-.5);
+            else if(leftTrigger.isPressed()){
+                horizontalRunning = true;
+                robot.getIntakeBottom().setPower(1);
             }
             else {
-                robot.getIntakeBottomRight().setPower(0);
-                robot.getIntakeTopRight().setPower(0);
+                robot.getIntakeBottom().setPower(0);
+            }
+            if(leftBumper.isPressed()){
+                horizontalRunning = true;
                 robot.getIntakeBottomLeft().setPower(0);
-                robot.getIntakeTopLeft().setPower(0);
+                robot.getIntakeBottomRight().setPower(0);
+                robot.getIntakeTop().setPower(1);
+            }
+            else if(rightBumper.isPressed()){
+                horizontalRunning = true;
+                robot.getIntakeTop().setPower(-1);
+            }
+            else {
+                robot.getIntakeTop().setPower(0);
+            }
+            if(!horizontalRunning){
+                if(aButton.isPressed()){
+                    robot.getIntakeBottomRight().setPower(.5);
+                    robot.getIntakeTopRight().setPower(.5);
+                    robot.getIntakeBottomLeft().setPower(.5);
+                    robot.getIntakeTopLeft().setPower(.5);
+                }
+                else if(bButton.isPressed()){
+                    robot.getIntakeBottomRight().setPower(-.5);
+                    robot.getIntakeTopRight().setPower(-.5);
+                    robot.getIntakeBottomLeft().setPower(-.5);
+                    robot.getIntakeTopLeft().setPower(-.5);
+                }
+                else {
+                    robot.getIntakeBottomRight().setPower(0);
+                    robot.getIntakeTopRight().setPower(0);
+                    robot.getIntakeBottomLeft().setPower(0);
+                    robot.getIntakeTopLeft().setPower(0);
+                }
+            }
+
+            if(dPadUp.isPressed()){
+                robot.getIntakeVertical().setPower(LIFT_RAISE);
+            }
+            else if(dPadDown.isPressed()){
+                robot.getIntakeVertical().setPower(-LIFT_RAISE);
+            }
+            else {
+                robot.getIntakeVertical().setPower(LIFT_VERTICAL_REST);
+            }
+            if(driverAButton.isPressed()&&!sounds.get(0).isPlaying()){
+                sounds.get(0).start();
+                sounds.get(1).stop();
+            }
+            else if(driverBButton.isPressed()&&!sounds.get(1).isPlaying()){
+                sounds.get(0).stop();
+                sounds.get(1).start();
+            }
+            else if(driverXButton.isPressed()){
+                sounds.get(0).stop();
+                sounds.get(1).stop();
             }
         }
-
-        if(dPadUp.isPressed()){
-            robot.getIntakeVertical().setPower(LIFT_RAISE);
-        }
-        else if(dPadDown.isPressed()){
-            robot.getIntakeVertical().setPower(-LIFT_RAISE);
-        }
-        else {
-            robot.getIntakeVertical().setPower(LIFT_VERTICAL_REST);
-        }
-    }
-
-    @Override
-    public void stop() {
-        //stop
     }
 }
